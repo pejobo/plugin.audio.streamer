7ef32b91b41ff0ac477367df23234f1d3e3fc5e9
Peter Jodeleit <jodeleit@e-spirit.com>
2017-10-15 12:06:28 +0200

    ignore git dir when building

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-echo $0
+#!/usr/bin/env bash
 rm lib/*.pyc
 python2 -m py_compile lib/*.py
 cd ..
-find plugin.audio.streamer -type f | grep -v ".*vscode.*" | grep -v ".*build.sh" | grep -v ".*test.py.*" | grep -v ".*zip" | zip plugin.audio.streamer.0.0.1.zip -@
+find plugin.audio.streamer -type f | grep -v ".git.*" | grep -v ".vscode.*" | grep -v "build.sh" | grep -v "test.py.*" | zip plugin.audio.streamer.0.0.1.zip -@
 cd plugin.audio.streamer
